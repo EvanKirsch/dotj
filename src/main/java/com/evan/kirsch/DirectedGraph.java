fa3222471b3c6b26f013c61da2a12ed19da8d934
Evan <evan@ivanhoe.attlocal.net>
2023-08-28 08:50:46 -0400

    fix git ignore

--- src/main/java/com/evan/kirsch/DirectedGraph.java
+++ src/main/java/com/evan/kirsch/DirectedGraph.java
@@ -4,25 +4,30 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 public class DirectedGraph<T> {
 
   private Map<T, List<T>> graph = new HashMap<>();
+  private String name;
 
   public void addNode(T n) {
     graph.put(n, new LinkedList<T>());
   }
 
-  public void addEdge(T source, T destination) {
+  public void addEdge(Edge<T> edge) {
 
-    if(!graph.containsKey(source)) {
-      graph.put(source, new LinkedList<T>());
+    if(!graph.containsKey(edge.getSource())) {
+      addNode(edge.getSource());
     }
-    if(!graph.containsKey(destination)) {
-      graph.put(destination, new LinkedList<T>());
+    if(!graph.containsKey(edge.getDestination())) {
+      addNode(edge.getDestination());
     }
 
-    graph.get(source).add(destination);
+    graph.get(edge.getSource()).add(edge.getDestination());
   }
 
 }
--- src/main/java/com/evan/kirsch/Dotj.java
+++ src/main/java/com/evan/kirsch/Dotj.java
@@ -6,8 +6,9 @@ import java.io.FileReader;
 
 public class Dotj {
 
+    private static final String EDGE_SYMBOL = "->";
+
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
         try {
             System.out.println(parseDotFile(args[0]));
         } catch (Exception e) {
@@ -18,14 +19,44 @@ public class Dotj {
 
     public static DirectedGraph<String> parseDotFile(String filePath) throws FileNotFoundException {
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        DirectedGraph<String> graph = null;
+        DirectedGraph<String> graph = new DirectedGraph<>();
 
         try {
+            String line = reader.readLine();
+
+            while (line != null) {
+                if(graph.getName() == null) {
+                    graph.setName(line);
+                } else if (isEdge(line)) {
+                    Edge<String> edge = getEdge(line);
+                    graph.addEdge(edge);
+                }
+                line = reader.readLine();
+            }
 
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
 
         return graph;
+    }
+
+    private static Edge<String> getEdge(String line) {
+        String[] nodes = line.split(EDGE_SYMBOL);
+        String source = getNodeName(nodes[0]);
+        String destination = getNodeName(nodes[1]);
+        return new Edge<>(source, destination);
+    }
+
+    private static String getNodeName(String node) {
+        String nName = node.trim();
+        nName = nName.replace("\"", "");
+        nName = nName.replace(";", "");
+        nName = nName.replaceAll("\\(([^\\)]+)\\)", "");
+        return nName;
+    }
+
+    private static boolean isEdge(String line) {
+        return line.contains(EDGE_SYMBOL);
     }
 }
